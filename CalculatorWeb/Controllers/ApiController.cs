cb2df65f747e20e8535c51e6b531119490132e6c
Pavel Krcho <pavlikusss@email.cz>
2024-10-09 15:49:52 +0200

    Vytvoření REST api

---
+++ CalculatorWeb/Controllers/ApiController.cs
@@ -0,0 +1,28 @@
+﻿using CalculatorWeb.Models.Api;
+using CalculatorWeb.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorWeb.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly ICalculationService _calculationService;
+
+        public ApiController(ICalculationService calculationService)
+        {
+            _calculationService = calculationService;
+        }
+
+        [HttpGet("evaluate")]
+        public IActionResult Evaluate(string expression)
+        {
+            var result = _calculationService.Evaluate(expression);
+            
+            var response = new EvaluateResponse() { Result = result };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ CalculatorWeb/Models/Api/EvaluateResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CalculatorWeb.Models.Api
+{
+    public class EvaluateResponse
+    {
+        [JsonPropertyName("result")]
+        public required string Result { get; set; }
+    }
+}
